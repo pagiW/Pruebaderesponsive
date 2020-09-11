fd2c2a4a246a3d7b704be133ca7ad780baeaa630
Juansito_the_dev <tecnosebastube@gmail.com>
2020-09-11 09:34:06 -0500

    commit cambio

--- code.js
+++ code.js
@@ -3,15 +3,32 @@ var option = document.getElementById("op");
 var header = document.getElementById("head");
 var main = document.getElementById("event");
 var pantalla = window.matchMedia("screen and (max-width: 425px)");
+var presi = document.getElementById("pressiona");
 var state = 0;
 
 pantalla.addListener(cambio);
 
 function cambio(evento) {
-    if (evento.matches) {
-        header.style.background = "blue";
+    if (evento.matches == true) {
+        presi.addEventListener('click', presionar);
+        header.innerHTML += "<p id='par'></p>";
     }
     else {
-        header.style.background = "green";
+        presi.removeEventListener('click', presionar);
+    }
+}
+cambio(pantalla);
+
+function presionar() {
+    var p = document.getElementById("par");
+    if (state == 0) {
+        header.style.backgroundColor = "blue";
+        p.innerHTML = "BLUE";
+        state = 1;
+    }
+    else {
+        header.style.backgroundColor = "green";
+        p.innerHTML = "GREEN";
+        state = 0;
     }
 }
